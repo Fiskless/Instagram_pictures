8d3cb756f48108ae13a5ab22aa9b13be05b5783d
Fiskless <70478482+Fiskless@users.noreply.github.com>
2020-09-19 15:56:28 +0500

    2 function in 1
    Make 1 funvtion which download pictures from spacex

--- Instagram_pictures.py
+++ Instagram_pictures.py
@@ -1,21 +1,6 @@
 import requests
 import os
 
-
-def load_picture(picture_name, url):
-
-    if not os.path.exists('images'):
-        os.makedirs('images')
-
-    filename = f'images/{picture_name}'
-
-    response = requests.get(url)
-    response.raise_for_status()
-
-    with open(filename, 'wb') as file:
-        file.write(response.content)
-
-
 def fetch_spacex_last_launch(url_from_spacexdata):
 
     response = requests.get(url_from_spacexdata)
@@ -23,7 +8,14 @@ def fetch_spacex_last_launch(url_from_spacexdata):
     picture_url = response.json()['links']['flickr']['original']
 
     for picture_index, url_picture in enumerate(picture_url):
-        load_picture(f'spacex{picture_index+1}.jpg', url_picture)
+        if not os.path.exists('images'):
+            os.makedirs('images')
+        filename = f'images/spacex{picture_index+1}.jpg'
+        response = requests.get(url_picture)
+        response.raise_for_status()
+        with open(filename, 'wb') as file:
+            file.write(response.content)
+
 
 
 fetch_spacex_last_launch('https://api.spacexdata.com/v4/launches/5eb87ce8ffd86e000604b33c')
