94589b63102fc7e02d50b8abc4f56574b331ab6e
Fiskless <70478482+Fiskless@users.noreply.github.com>
2020-09-22 23:35:53 +0500

    .gitignore is now working

--- Instagram_pictures.py
+++ Instagram_pictures.py
@@ -55,20 +55,20 @@ def hubble_pictures_load(id):
 if __name__ == '__main__':
 
 
-    load_dotenv()
-    instagram_login = os.getenv("INSTAGRAM_LOGIN")
-    instagram_password = os.getenv("INSTAGRAM_PASSWORD")
-
-    urllib3.disable_warnings()
-
-    fetch_spacex_last_launch('https://api.spacexdata.com/v4/launches/5eb87ce8ffd86e000604b33c')
-
-    response = requests.get('http://hubblesite.org/api/v3/images/holiday_cards')
-    response.raise_for_status()
-    collection = response.json()
-    for picture_index, pictures_id in enumerate(collection):
-        collection_pictures_id = response.json()[picture_index]['id']
-        hubble_pictures_load(collection_pictures_id)
+    # load_dotenv()
+    # instagram_login = os.getenv("INSTAGRAM_LOGIN")
+    # instagram_password = os.getenv("INSTAGRAM_PASSWORD")
+    #
+    # urllib3.disable_warnings()
+    #
+    # fetch_spacex_last_launch('https://api.spacexdata.com/v4/launches/5eb87ce8ffd86e000604b33c')
+    #
+    # response = requests.get('http://hubblesite.org/api/v3/images/holiday_cards')
+    # response.raise_for_status()
+    # collection = response.json()
+    # for picture_index, pictures_id in enumerate(collection):
+    #     collection_pictures_id = response.json()[picture_index]['id']
+    #     hubble_pictures_load(collection_pictures_id)
 
     bot = Bot()
     bot.login(username="instagram_login", password="instagram_password")
