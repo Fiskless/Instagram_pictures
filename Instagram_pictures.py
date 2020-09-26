4d5867ff16e8be2b0d4e96aa537d5223a6b29da7
Fiskless <70478482+Fiskless@users.noreply.github.com>
2020-09-26 16:06:05 +0500

    Update Instagram_pictures.py

--- Instagram_pictures.py
+++ Instagram_pictures.py
@@ -16,15 +16,13 @@ def fetch_spacex_last_launch(spacex_url):
     pictures_url = response.json()['links']['flickr']['original']
 
     for picture_index, url_picture in enumerate(pictures_url):
-        if not os.path.exists('images'):
-            os.makedirs('images')
+
         filename = f'images/spacex{picture_index+1}.jpg'
         response = requests.get(url_picture)
         response.raise_for_status()
         with open(filename, 'wb') as file:
             file.write(response.content)
-        if not os.path.exists('images_for_instagram'):
-            os.makedirs('images_for_instagram')
+
         image = Image.open(f"images/spacex{picture_index+1}.jpg")
         image.thumbnail((1080, 1080))
         image_convert = image.convert('RGB')
@@ -37,15 +35,12 @@ def hubble_pictures_load(id):
     response.raise_for_status()
     hubble_picture_url = response.json()['image_files'][-1]['file_url']
     expansion = hubble_picture_url.split('.')[-1]
-    if not os.path.exists('images'):
-        os.makedirs('images')
     filename = f'images/image_{id}.{expansion}'
     response = requests.get(f'https:{hubble_picture_url}', verify = False)
     response.raise_for_status()
     with open(filename, 'wb') as file:
         file.write(response.content)
-    if not os.path.exists('images_for_instagram'):
-        os.makedirs('images_for_instagram')
+        
     image = Image.open(f"images/image_{collection_pictures_id}.{expansion}")
     image.thumbnail((1080, 1080))
     image_convert = image.convert('RGB')
@@ -58,6 +53,12 @@ if __name__ == '__main__':
     load_dotenv()
     instagram_login = os.getenv("INSTAGRAM_LOGIN")
     instagram_password = os.getenv("INSTAGRAM_PASSWORD")
+
+    if not os.path.exists('images'):
+            os.makedirs('images')
+    if not os.path.exists('images_for_instagram'):
+        os.makedirs('images_for_instagram')
+
 
     fetch_spacex_last_launch('https://api.spacexdata.com/v4/launches/5eb87ce8ffd86e000604b33c')
 
